61f7993ca2c3ffdb3edc644b3781bb5e871337d8
leonimurilo <leonimega@hotmail.com>
2017-07-29 20:30:55 -0300

    Use Field component from redux-form inside PostsNew component

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -3,14 +3,51 @@
  */
 import React, {Component} from "react";
 
+// Field is a component. We tell what type of input we want to receive from the user
+// the problem is that it doesn't know how to show up to the user. it only knows how to interact with redux-form
+// reduxForm is a function that is very similar to the connect function of the react-redux
+// This function is what allows our component to communicate with that additional reducer called form
+import {Field, reduxForm} from "redux-form";
+
+// lets user to create (post) a new post
 class PostsNew extends Component{
-    render(){
+
+    // function responsible to render the input/field for the user
+    // its parameter called field (that is passed by the redux-form) contains some event handlers
+    // the field is a way to bind an input to the Field saying "hey, Field (from redux-form)
+    // this is the input that you are responsible"
+    renderTitleField(field){
         return (
             <div>
-                PostsNew!
+                {/* tells Field that this is the input it is responsible for*/}
+                {/* the 3 dots indicate that every single property of the field.input must communicate
+                as props to the input
+                */}
+                <input
+                    type="text"
+                    {...field.input}
+                />
             </div>
         );
     }
+
+    render(){
+        return (
+            <form>
+                {/*The name property specifies what piece of state is being edited*/}
+                <Field
+                    name="title"
+                    component={this.renderTitleField}
+                />
+            </form>
+        );
+    }
+
 }
 
-export default PostsNew;
+//reduxForm() give redux-form the ability to communicate directly from this component to the reducer
+// (reducer that came from the redux-form library and that was put into the app rootReducer)
+// receives a single argument in the first pair of parenthesis. It is an options object
+export default reduxForm({
+        form: "PostsNewForm" // unique name
+})(PostsNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux';
 import PostsReducer from "./Posts";
 
+// redux form is just saving us from having to create a bunch of action creators
+// when we use the Field component inside our components, they already take care about firing the needed actions
+import {reducer as formReducer} from "redux-form";
+
 const rootReducer = combineReducers({
-  posts: PostsReducer
+    posts: PostsReducer,
+    form: formReducer
 });
 
 export default rootReducer;
