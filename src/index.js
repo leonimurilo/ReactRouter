ced36f225546967fbb75fe202690fb1536df433f
leonimurilo <leonimega@hotmail.com>
2017-07-29 13:28:11 -0300

    testing react router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
-import reducers from './reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import {Provider} from "react-redux";
+import {applyMiddleware, createStore} from "redux";
+// BrowserRouter interacts with the history library and decides what do do based on the change of the url
+// Route is a react component that we can render inside of any other react component
+// the purpose of Router is to provide a configuration that will define which component will be shown
+// depending on the url
+import {BrowserRouter, Route} from "react-router-dom";
+import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Hello extends React.Component {
+    render() { return <div>Hello!</div>}
+}
+
+class Goodbye extends React.Component {
+    render() { return <div>Goodbye!</div>}
+}
+
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <div>
+                <div>Header</div>
+                {/*if user wants to go to the route specified in path, then it will show the component*/}
+                <Route path="/hello" component={Hello}/>
+                <Route path="/goodbye" component={Goodbye}/>
+            </div>
+        </BrowserRouter>
+    </Provider>
+    , document.querySelector('.container'));
