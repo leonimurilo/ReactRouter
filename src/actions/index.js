913b8fa20b1418dc8ac8de1d6215fbd805850cdb
leonimurilo <leonimega@hotmail.com>
2017-07-29 17:11:15 -0300

    display posts on home screen

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,6 @@ const API_KEY = "?key=leonimurilo";
 
 export function fetchPosts() {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
-
     return {
         type: FETCH_POSTS,
         payload: request
--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -3,26 +3,42 @@
  */
 import React, {Component} from "react";
 import {connect} from "react-redux";
-import {fetchPosts} from "../actions"
+import {fetchPosts} from "../actions";
+import _ from "lodash";
 
 
 class PostsIndex extends Component {
 
-
+    renderPosts(posts){
+        return _.map(posts, post => {
+            return (
+                <li className="list-group-item" key={post.id}>
+                    {post.title}
+                </li>
+            );
+        });
+    }
 
     render(){
+        console.log(this.props.posts);
         return (
             <div>
-                Posts Index
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts(this.props.posts)}
+                </ul>
             </div>
         );
     }
 
     componentDidMount(){
-        this.props;fetchPosts();
+        this.props.fetchPosts();
     }
 }
 
+function mapStateToProps({posts}){
+    return {posts};
+}
 
 // instead of using mapDispatchToProps, we can pass the action creator directly and the connect() takes care for us
-export default connect(null, {fetchPosts})(PostsIndex);
+export default connect(mapStateToProps, {fetchPosts})(PostsIndex);
