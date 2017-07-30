dc268bcd30c26bbfa03c8209e323bacda0cd1d31
leonimurilo <leonimega@hotmail.com>
2017-07-30 11:33:24 -0300

    add new action creator for fetching a single post. add a new case inside posts reducer.

--- src/reducers/Posts.js
+++ src/reducers/Posts.js
@@ -1,13 +1,25 @@
 /**
  * Created by Leoni on 7/29/2017.
  */
-import {FETCH_POSTS} from "../actions/index";
+import {FETCH_POSTS, FETCH_POST} from "../actions/index";
 import _ from "lodash";
 
 export default function (state = null, action) {
     switch(action.type){
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, "id");
+        case FETCH_POST:
+            // ES5
+            // const post = action.payload.data;
+            // const newState = {...state, };
+            // newState[post.id] = post;
+            // return newState;
+
+            // ES6
+            // add a new property to state. the key is the id (inside brackets) and the value after the ":"
+            // this creates a new instance of state too.
+            return {...state, [action.payload.data.id]: action.payload.data};
+
         default:
             return state;
     }
